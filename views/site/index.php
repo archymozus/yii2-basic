675e652f7fbfac5ba2b314b050a0d306500cd71a
archy <archy@archy-desktop>
2017-08-29 13:04:40 +0300

    master branch

--- views/site/index.php
+++ views/site/index.php
@@ -1,46 +1,15 @@
-<div class="row">
-    <div class="col-sm-2">
 <?php
 
-/* @var $this yii\web\View */
-
-use yii\bootstrap\Nav;
-
-echo Nav::widget(
-        [
-            'items' => [
-                [
-                    'label' => 'Ссылка 1',
-                    'url' => ['#']
-                ],
-                [
-                    'label' => 'Ссылка 2',
-                    'url' => ['#'],
-                ],
-                [
-                    'label' => 'Выпадающий список',
-                    'item' => [
-                        [
-                            'label' => 'Ссылка 1',
-                            'url' => ['#'],
-                        ],
-                        [
-                            'label' => 'Ссылка 2',
-                            'url' => ['#'],
-                        ],
-                        ]
-                ],
-            ]
-        ]
-    );
-
-$this->title = 'My Yii Application';
+namespace app\controllers;
 ?>
-         </div>
-         <div class="col-sm-10">
-             ///////4<br>
-                ///////5<br>
-                ///////6<br>
-          </div>
+<div class="row">
+    <div class="col-sm-2">
+        <?php
+        SidebarController::Viewsidebar();
+        ?>
+    </div>
+    <div class="col-sm-10">
+        <h1>Добро пожаловать</h1>
+    </div>
 </div>
 
