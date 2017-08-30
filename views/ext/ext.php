649e069a19b527a41f1edb7491197a85507c503b
archy <archy@archy-desktop>
2017-08-30 14:03:07 +0300

    Add Extensions

--- views/ext/ext.php
+++ views/ext/ext.php
@@ -1,21 +1,31 @@
 <?php
-
-use yii\helpers\Html;
-use yii\widgets\LinkPager;
-use app\models\Extensions;
-use app\models\Extension;
-
+namespace app\controllers;
 ?>
-<h1>Extensions</h1>
-<ul>
-<?php 
-
-foreach ($extension[0] as $key => $value) {
-    
-       print_r($key ." : ". $value. "\r\n");?>
-       <br>
+<div class="row">
+    <div class="col-sm-2">
+        <?php
+        SidebarController::Viewsidebar();
+        ?>
+    </div>
+    <div class="col-sm-offset-2">        
+    </div>
+    <div class="col-sm-8">
         
-    
-<?php } ?>
-</ul>
+        <h1>Extensions</h1>
+        <ul>
+            <?php
+            use yii\helpers\Html;
+            use yii\widgets\LinkPager;
+            use app\models\Extensions;
+            foreach ($extension[0] as $key => $value) {
+
+                print_r($key . " : " . $value . "\r\n");
+                ?>
+                <br>
+
+
+            <?php } ?>
+        </ul>
+    </div>
+</div>
 
